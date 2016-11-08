43a0c2372dc677dd4ea848b4238e91b6b74ecc33
Hackerman64 <jonahlharris@utexas.edu>
2016-11-08 15:58:35 -0600

    Controller Layout

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -21,12 +21,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -94,13 +99,30 @@ public class Main extends Application {
     	   }    
     	}
     
- 	
+ 	//Jonah
 	 @Override
 	 public void start(Stage primaryStage) throws ClassNotFoundException {
+		 
+		 //Initial creation of most major controls and layouts
+		 new SecondStage();
+		 BorderPane borders = new BorderPane();
+		 HBox top = new HBox();
+		 HBox bottom = new HBox();
+	 	 VBox left = new VBox();
+	 	 VBox right = new VBox();
+	 	 GridPane center = new GridPane();
+         primaryStage.setTitle("Welcome to Critters!");
+         Button show = new Button("Show");
+	     Button make = new Button("Make");
+	     Button seed = new Button("Set Seed");
+	     Button step = new Button("Run Time Step");
+	     Button quit = new Button("Quit");
+	     Button animate = new Button("Animate");
+		 
+	     // Obtain Critter subclasses for make
 		 String workingDir = System.getProperty("user.dir") + "/src/assignment5";	
 		 File myCritters = new File(workingDir);
 		 listFilesForFolder(myCritters);
-
 		 	for (int k = 0; k < numberOfFiles - 1; k += 1) {
 		 		try {
 		 			int cutoff = classNames[k].indexOf(".");
@@ -115,57 +137,40 @@ public class Main extends Application {
 		 			// Do nothing; name is not a class or a subclass of critter		
 		 		}
 		 	}
-		 	
 		 	critterNames = new String[numberOfCritters];
 		 	for (int i = 0; i < numberOfCritters; i += 1){
 		 		critterNames[i] = classNames[i];
 		 	}
-		 	
-		 	
-		 	final TitledPane[] tps = new TitledPane[numberOfCritters];
-		 	new SecondStage();
-	    	GridPane myLayout = new GridPane();
-	        primaryStage.setTitle("Welcome to Critters!");
-	        Button show = new Button("Show");
-	        Button make = new Button("Make");
-	        Button seed = new Button("Set Seed");
-	        Button step = new Button("Run Time Step");
-	        Button quit = new Button("Quit");
-	        Button animate = new Button("Animate");
-	        
 	        ObservableList<String> differentCritters = FXCollections.observableArrayList();
 	        for (int i = 0; i < numberOfCritters; i += 1) {
 	        	differentCritters.add(critterNames[i]);
 	        }
+	        
+	        // ComboBox for critter selection (make)
 	        final ComboBox<String> listOfCritters = new ComboBox<>(differentCritters);
+	        listOfCritters.setValue(critterNames[0]);
 	        
-	        // Slider for time steps
-//	        Slider slider = new Slider();
-//	        slider.setMin(1);
-//	        slider.setMax(100);
-//	        slider.setValue(40);
-//	        slider.setShowTickLabels(true);
-//	        slider.setShowTickMarks(true);
-//	        slider.setMajorTickUnit(50);
-//	        slider.setMinorTickCount(5);
-//	        slider.setBlockIncrement(10);
-	        
-	        // Tilted Pane
-	                               
-	        final Accordion critterStatsList = new Accordion();        
-	        
+	        // Accordion for show statistics
+	        final Accordion critterStatsList = new Accordion();
+	        final TitledPane[] tps = new TitledPane[numberOfCritters];
 	        for (int i = 0; i < numberOfCritters; i += 1) { 
 	        		String critter_stats = updateStats(critterNames[i]);
 	        	    tps[i] = new TitledPane();
-	        	    tps[i].setText(critterNames[i] + " Statistics");
+	        	    tps[i].setText(critterNames[i] + " Statistics (" + critterNames[i].toString() + ")");
 	        	    tps[i].setContent(new Label("\n" + critter_stats));
 	        }   
 	        critterStatsList.getPanes().addAll(tps);
 	        critterStatsList.setExpandedPane(tps[0]);
 	        
+	        // TextFields for integer input (make and step)
 	        TextField number_critters = new TextField();
+	        number_critters.setPromptText("Number of Critters...");
 	        TextField step_number = new TextField();
+	        step_number.setPromptText("Number of steps...");
+	        TextField seed_number = new TextField();
+	        seed_number.setPromptText("Seed value...");
 	        
+	        // Button Action Handling
 	        show.setOnAction(new EventHandler<ActionEvent>() {
 	            @Override
 	            public void handle(ActionEvent event) {
@@ -189,7 +194,8 @@ public class Main extends Application {
 		        		String critter_stats = updateStats(critterNames[i]);
 		        	    tps[i].setText(critterNames[i] + " Statistics");
 		        	    tps[i].setContent(new Label("\n" + critter_stats));
-	        		} 
+	        		}
+	        		Critter.displayWorld();
 	        	}
 	        });
 	        step.setOnAction(new EventHandler<ActionEvent>() {
@@ -206,6 +212,22 @@ public class Main extends Application {
 		        	    tps[i].setText(critterNames[i] + " Statistics");
 		        	    tps[i].setContent(new Label("\n" + critter_stats));
 	        		} 
+	        		Critter.displayWorld();
+	        	}
+	        });
+	        seed.setOnAction(new EventHandler<ActionEvent>() {
+	        	@Override
+	        	public void handle(ActionEvent event) {
+	        		long val = Long.parseLong(step_number.getText());
+	        		for (int i = 0; i < val; i += 1){
+	        			Critter.setSeed(val);
+	        		}
+	        		for (int i = 0; i < numberOfCritters; i += 1) { 
+		        		String critter_stats = updateStats(critterNames[i]);
+		        	    tps[i].setText(critterNames[i] + " Statistics");
+		        	    tps[i].setContent(new Label("\n" + critter_stats));
+	        		} 
+	        		Critter.displayWorld();
 	        	}
 	        });
 	        quit.setOnAction(new EventHandler<ActionEvent>() {
@@ -222,18 +244,77 @@ public class Main extends Application {
 	        	}
 	        });
 	        
-	        myLayout.add(show, 0, 0);
-	        myLayout.add(number_critters, 0, 1);
-	        myLayout.add(make, 5, 1);
-	        myLayout.add(step, 5, 7);
-	        myLayout.add(step_number, 0, 7);
-	        myLayout.add(quit, 0, 25);
-	        myLayout.add(animate,  0, 30);
-	        myLayout.add(critterStatsList, 0, 20);
-	        myLayout.add(listOfCritters, 0, 2);
+	        // Find size of screen and set window sizes
+	        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+	        primaryStage.setX(primaryScreenBounds.getMinX());
+	        primaryStage.setY(primaryScreenBounds.getMinY());
+	        primaryStage.setWidth(primaryScreenBounds.getWidth()/2);
+	        primaryStage.setHeight(primaryScreenBounds.getHeight()/2);
 	        
-	        primaryStage.setScene(new Scene(myLayout, 300, 250));
+	        
+	        // Extra code (may use)
+//	        myLayout.setMaxSize(primaryScreenBounds.getHeight(), primaryScreenBounds.getWidth());
+//	        myLayout.add(number_critters, 0, 1);
+//	        myLayout.add(make, 5, 1);
+//	        myLayout.add(step, 5, 7);
+//	        myLayout.add(step_number, 0, 7);
+//	        myLayout.add(quit, (int)primaryScreenBounds.getHeight(), (int)primaryScreenBounds.getWidth());
+//	        myLayout.add(animate,  0, 30);
+//	        //myLayout.add(critterStatsList, 0, 20);
+//	        myLayout.add(listOfCritters, 0, 2);
+	        
+	        // Slider for time steps
+//	        Slider slider = new Slider();
+//	        slider.setMin(1);
+//	        slider.setMax(100);
+//	        slider.setValue(40);
+//	        slider.setShowTickLabels(true);
+//	        slider.setShowTickMarks(true);
+//	        slider.setMajorTickUnit(50);
+//	        slider.setMinorTickCount(5);
+//	        slider.setBlockIncrement(10);
+	        
+	        top.setSpacing(10);
+	        bottom.setSpacing(10);
+	        left.setSpacing(10);
+	        right.setSpacing(10);
+	        center.setHgap(10);
+	        center.setVgap(10);
+	        
+	        top.setPadding(new Insets(15, 12, 15, 12));
+	        bottom.setPadding(new Insets(15, 12, 15, 12));
+	        left.setPadding(new Insets(15, 12, 15, 12));
+	        right.setPadding(new Insets(15, 12, 15, 12));
+	        
+	        int row = 0;
+	        top.getChildren().addAll(listOfCritters, number_critters, make);
+	        bottom.getChildren().addAll(quit);
+	        left.getChildren().addAll(seed);
+	        center.setPadding(new Insets(15, 12, 15, 12));
+	        center.add(new Label("Make Critters"), 0, row);
+	        center.add(listOfCritters, 0, row + 1);
+	        center.add(number_critters, 1, row + 1);
+	        center.add(make, 2, row + 1);
+	        center.add(new Label("Critter Motion Handling"), 0, row + 4);
+	        center.add(step_number, 1, row + 5);
+	        center.add(step, 2, row + 5);
+	        center.add(animate, 2, row + 6);
+	        // Add a slider if we can
+	        center.add(new Label("Set Seed"), 0, row + 9);
+	        center.add(seed_number, 1, row + 10);
+	        center.add(seed, 2, row + 10);
+	        center.add(quit, 10, row + 12);
+	        
+	        // Final steps to display
+	        borders.setTop(top);
+	        borders.setBottom(bottom);
+	        borders.setRight(right);
+	        borders.setLeft(left);
+	        //borders.setCenter(center);
+	        
+	        primaryStage.setScene(new Scene(center, primaryScreenBounds.getWidth()/2, primaryScreenBounds.getHeight()));
 	        primaryStage.show();
+	        Critter.displayWorld();
 	    }
 
 	public static void main(String[] args) {
